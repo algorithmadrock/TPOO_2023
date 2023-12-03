7d938e40d80e5caa9d0d856def1032711f3fdf39
MuriloFerreira1 <rillofelix@hotmail.com>
2023-12-03 16:57:02 -0300

    Adicionando emprestimo e funcionario à tela principal

--- src/view/TelaPrincipal.java
+++ src/view/TelaPrincipal.java
@@ -12,6 +12,8 @@ import javafx.stage.Stage;
 
 public class TelaPrincipal extends Application{
 	
+	
+	
 	@Override
 	public void start(Stage stage) throws Exception {
 		MenuBar barraMenu = new MenuBar();
@@ -20,7 +22,9 @@ public class TelaPrincipal extends Application{
 		Menu mCliente = new Menu("Cliente");
 		Menu mDevolucao = new Menu("Devolucao");
 		Menu mEditora = new Menu("Editora");
+		Menu mEmprestimo = new Menu("Emprestimo");
 		Menu mFornecedor = new Menu("Fornecedor");
+		Menu mFuncionario = new Menu("Funcionario");
 		Menu mLivro = new Menu("Livro");
 		Menu mVenda = new Menu("Venda");
 		MenuItem autor = new MenuItem("Autor");
@@ -28,7 +32,9 @@ public class TelaPrincipal extends Application{
 		MenuItem cliente = new MenuItem("Cliente");
 		MenuItem devolucao = new MenuItem("Devolucao");
 		MenuItem editora = new MenuItem("Editora");
+		MenuItem emprestimo = new MenuItem("Emprestimo");
 		MenuItem fornecedor = new MenuItem("Fornecedor");
+		MenuItem funcionario = new MenuItem("Funcionario");
 		MenuItem livro = new MenuItem("Livro");
 		MenuItem venda = new MenuItem("Venda");
 		mAutor.getItems().add(autor);
@@ -36,12 +42,14 @@ public class TelaPrincipal extends Application{
 		mCliente.getItems().add(cliente);
 		mDevolucao.getItems().add(devolucao);
 		mEditora.getItems().add(editora);
+		mEmprestimo.getItems().add(emprestimo);
 		mFornecedor.getItems().add(fornecedor);
+		mFuncionario.getItems().add(funcionario);
 		mLivro.getItems().add(livro);
 		mVenda.getItems().add(venda);
 		
 		
-		barraMenu.getMenus().addAll(mAutor,mCategoria,mCliente,mDevolucao,mEditora,mFornecedor,mLivro,mVenda);
+		barraMenu.getMenus().addAll(mAutor,mCategoria,mCliente,mDevolucao,mEditora, mEmprestimo,mFornecedor, mFuncionario,mLivro,mVenda);
 		
 		BorderPane painel = new BorderPane();
 		painel.setTop(barraMenu);
