0f0d071f8b912ac09972c67831fa715c6e5925e2
Luiza <luizamfelix@gmail.com>
2023-12-03 15:25:08 -0300

    fornecedor, devolucao, vendas

--- src/entity/Fornecedor.java
+++ src/entity/Fornecedor.java
@@ -1,5 +1,49 @@
 package entity;
 
+import java.util.Set;
+
 public class Fornecedor {
+	private static int id;
+	private String cnpj;
+	private String nome;
+	private boolean status;
+	private Set<Livro> livros;
+	
+	public Fornecedor() {
+		id++;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public Set<Livro> getLivros() {
+		return livros;
+	}
+	public void setLivros(Livro livro) {
+		this.livros.add(livro);
+	}
+	
+	
 
 }
