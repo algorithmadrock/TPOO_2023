e1cbcbc6744e824c4e9b1a088c7f640a256d37a6
Maicon Alves Guedes <maicon.ag17@hotmail.com>
2023-12-05 22:53:48 -0300

    Att DAO

--- src/view/TelaAutor.java
+++ src/view/TelaAutor.java
@@ -19,53 +19,53 @@ import javafx.util.StringConverter;
 import javafx.util.converter.LocalDateStringConverter;
 import javafx.util.converter.NumberStringConverter;
 
-public class TelaAutor implements TelaMudar{
+public class TelaAutor implements TelaMudar {
 	private TableView<Autor> table = new TableView<>();
-	private TextField txtID = new TextField ();	
-	private TextField txtNome = new TextField ();
+	private TextField txtID = new TextField();
+	private TextField txtNome = new TextField();
 	private TextField txtNacionalidade = new TextField();
-	private TextField txtNascimento = new TextField ();
-	private Button btnAdicionar = new Button ("Adicionar");
-	private Button btnPesquisar = new Button ("Pesquisar");
+	private TextField txtNascimento = new TextField();
+	private Button btnAdicionar = new Button("Adicionar");
+	private Button btnPesquisar = new Button("Pesquisar");
 	private ControllerAutor control = new ControllerAutor();
 	private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	
+
 	public void generateBindings() {
 		Bindings.bindBidirectional(txtID.textProperty(), control.idProperty(), new NumberStringConverter());
 		Bindings.bindBidirectional(txtNome.textProperty(), control.nomeProperty());
 		Bindings.bindBidirectional(txtNacionalidade.textProperty(), control.nacionalidadeProperty());
-		StringConverter localDtf = new LocalDateStringConverter(dtf,dtf);
+		StringConverter localDtf = new LocalDateStringConverter(dtf, dtf);
 		Bindings.bindBidirectional(txtNascimento.textProperty(), control.nascimentoProperty(), localDtf);
 	}
-	
+
 	public void generateTable() {
 		table.setItems(control.getLista());
-		
+
 		TableColumn<Autor, Integer> colID = new TableColumn("Id");
-		colID.setCellValueFactory(new PropertyValueFactory<Autor,Integer>("id"));
-		
+		colID.setCellValueFactory(new PropertyValueFactory<Autor, Integer>("id"));
+
 		TableColumn<Autor, String> colNome = new TableColumn("Nome");
-		colNome.setCellValueFactory( itemData -> new ReadOnlyStringWrapper(itemData.getValue().getNome()));
-		
-		TableColumn<Autor, String> colNacionalidade = new TableColumn ("Nacionalidade");
-		colNacionalidade.setCellValueFactory( itemData -> new ReadOnlyStringWrapper(itemData.getValue().getNacionalidade()));
-		
+		colNome.setCellValueFactory(itemData -> new ReadOnlyStringWrapper(itemData.getValue().getNome()));
+
+		TableColumn<Autor, String> colNacionalidade = new TableColumn("Nacionalidade");
+		colNacionalidade
+				.setCellValueFactory(itemData -> new ReadOnlyStringWrapper(itemData.getValue().getNacionalidade()));
+
 		TableColumn<Autor, String> colNascimento = new TableColumn<>("Nascimento");
-		colNascimento.setCellValueFactory(itemData -> new ReadOnlyStringWrapper(dtf.format(itemData.getValue().getDataNascimento())));
-		
+		colNascimento.setCellValueFactory(
+				itemData -> new ReadOnlyStringWrapper(dtf.format(itemData.getValue().getDataNascimento())));
+
 		table.getColumns().addAll(colID, colNome, colNacionalidade, colNascimento);
 	}
-	
-	
-	
+
 	@Override
 	public Pane renderizaPainel() {
 		BorderPane panPrincipal = new BorderPane();
-		
+
 		txtID.setEditable(false);
-		
+
 		GridPane panFormulario = new GridPane();
-		panFormulario.add(new Label ("Id: "), 0, 0);
+		panFormulario.add(new Label("Id: "), 0, 0);
 		panFormulario.add(txtID, 1, 0);
 		panFormulario.add(new Label("Nome: "), 0, 1);
 		panFormulario.add(txtNome, 1, 1);
@@ -75,16 +75,16 @@ public class TelaAutor implements TelaMudar{
 		panFormulario.add(txtNascimento, 1, 3);
 		panFormulario.add(btnAdicionar, 0, 4);
 		panFormulario.add(btnPesquisar, 1, 4);
-		
+
 		btnAdicionar.setOnAction(e -> control.salvar());
 		btnPesquisar.setOnAction(e -> control.ler());
-		
+
 		panPrincipal.setCenter(table);
 		panPrincipal.setTop(panFormulario);
-		
+
 		generateBindings();
 		generateTable();
-		
+
 		return panPrincipal;
 	}
 
--- src/view/TelaPrincipal.java
+++ src/view/TelaPrincipal.java
@@ -1,7 +1,5 @@
 package view;
 
-
-
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
@@ -10,13 +8,11 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class TelaPrincipal extends Application{
-	
+public class TelaPrincipal extends Application {
+
 	private TelaCategoria telaCategoria = new TelaCategoria();
 	private TelaAutor telaAutor = new TelaAutor();
-	
-	
-	
+
 	@Override
 	public void start(Stage stage) throws Exception {
 		MenuBar barraMenu = new MenuBar();
@@ -50,10 +46,10 @@ public class TelaPrincipal extends Application{
 		mFuncionario.getItems().add(funcionario);
 		mLivro.getItems().add(livro);
 		mVenda.getItems().add(venda);
-		
-		
-		barraMenu.getMenus().addAll(mAutor,mCategoria,mCliente,mDevolucao,mEditora, mEmprestimo,mFornecedor, mFuncionario,mLivro,mVenda);
-		
+
+		barraMenu.getMenus().addAll(mAutor, mCategoria, mCliente, mDevolucao, mEditora, mEmprestimo, mFornecedor,
+				mFuncionario, mLivro, mVenda);
+
 		BorderPane painel = new BorderPane();
 		painel.setTop(barraMenu);
 		Scene cena = new Scene(painel);
@@ -61,7 +57,7 @@ public class TelaPrincipal extends Application{
 		stage.show();
 		painel.setCenter(telaAutor.renderizaPainel());
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
